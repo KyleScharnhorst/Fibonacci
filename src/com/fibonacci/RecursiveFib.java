7c22ea6da29fdc5be69c62d77eed351c6160b152
KyleScharnhorst <scharkr1@uw.edu>
2017-09-21 15:48:21 -0700

    Add: Recursive fib implementation.

--- src/com/fibonacci/RecursiveFib.java
+++ src/com/fibonacci/RecursiveFib.java
@@ -1,4 +1,18 @@
 package com.fibonacci;
 
 public class RecursiveFib {
+
+    public static int getFibElement(int k) {
+        if (k == 0) { return 0; }
+        if (k == 1) { return 1; }
+        return recurseFib(k);
+    }
+
+    private static int recurseFib(int n) {
+        if( n <= 1) {
+            return n;
+        }
+        return recurseFib(n-1) + recurseFib(n-2);
+    }
+
 }
