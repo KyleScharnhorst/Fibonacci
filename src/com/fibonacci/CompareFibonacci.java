1c7ef817076e52282b65f71d6fc965a7740e62c8
KyleScharnhorst <scharkr1@uw.edu>
2017-09-21 14:48:01 -0700

    Add: Dynamic fibonacci implementation.

--- src/com/fibonacci/CompareFibonacci.java
+++
@@ -1,9 +0,0 @@
-package com.fibonacci;
-
-//compares the speed of a basic recursive solution and
-public class CompareFibonacci {
-
-    public static void main(String[] args) {
-	// compare basic recursion vs
-    }
-}
---
+++ src/com/fibonacci/DynamicFib.java
@@ -0,0 +1,21 @@
+package com.fibonacci;
+
+public final class DynamicFib {
+    private static int[] _cache = null;
+
+    private DynamicFib(){}
+
+    public static int getFibElement(int k) {
+        if(k == 0) { return 0; }
+        if(k == 1) { return 1; }
+
+        _cache = new int[]{0,1};
+        for(int i = 2; i <= k; i++) {
+            int temp = _cache[0] + _cache[1];
+            _cache[0] = _cache[1];
+            _cache[1] = temp;
+        }
+        return _cache[1];
+    }
+
+}
