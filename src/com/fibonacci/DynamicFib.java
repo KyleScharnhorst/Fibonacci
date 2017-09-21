b594722f36553861e82564a0d60db7dd66fa0b32
KyleScharnhorst <scharkr1@uw.edu>
2017-09-21 16:33:40 -0700

    Add: dynamic fib solution and method to benchmark.

--- src/com/fibonacci/DynamicFib.java
+++ src/com/fibonacci/DynamicFib.java
@@ -1,4 +1,38 @@
 package com.fibonacci;
 
-public class DynamicFib {
+public final class DynamicFib {
+    private static int[] _cache;
+
+    private DynamicFib(){}
+
+    public static int getFibElement(int k) {
+        int result;
+        //check for base case before any work.
+        if(k <= 1) {
+            result = k;
+        } else {
+            //init cache
+            _cache = new int[k+1];
+            _cache[1] = 1;
+
+            //begin recursion
+            result = recurseFib(k);
+        }
+
+        return result;
+    }
+
+    private static int recurseFib(int n) {
+        int result;
+
+        if( n <= 1) {
+            result = n;
+        } else if( _cache[n] != 0 ) {
+            result = _cache[n];
+        } else {
+            _cache[n] = result = recurseFib(n-1) + recurseFib(n-2);
+        }
+
+        return result;
+    }
 }
--- src/com/fibonacci/Main.java
+++ src/com/fibonacci/Main.java
@@ -50,12 +50,38 @@ public class Main {
         System.out.println("Time to generate 46th Fibonacci element: " + avg + "ns");
     }
 
+    private static void testDynamicFib(int max_executions) {
+        long before;
+        long after;
+        long avg;
+
+        System.out.println("Dynamic fib result: " + DynamicFib.getFibElement(0));
+        System.out.println("Dynamic fib result: " + DynamicFib.getFibElement(1));
+        System.out.println("Dynamic fib result: " + DynamicFib.getFibElement(2));
+        System.out.println("Dynamic fib result: " + DynamicFib.getFibElement(5));
+        System.out.println("Dynamic fib result: " + DynamicFib.getFibElement(10));
+        System.out.println("Dynamic fib result: " + DynamicFib.getFibElement(46));
+
+        avg = 0;
+        for (int i = 0; i < max_executions; i++) {
+            before = System.nanoTime();
+            DynamicFib.getFibElement(46);
+            after = System.nanoTime();
+            avg += after - before;
+        }
+        avg /= max_executions;
+
+        System.out.println("Time to generate 46th Fibonacci element: " + avg + "ns");
+    }
+
     public static void main(String[] args) {
         // compare basic recursion vs dynamic fib
         int max_executions = 3;
 
         testIterativeFib(max_executions);
+        testDynamicFib(max_executions);
         testRecurseFib(max_executions);
-
     }
+
+
 }
