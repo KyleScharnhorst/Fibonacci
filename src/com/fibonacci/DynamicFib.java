70e22e72c2c1595284e27d3505984dfe71369ad3
KyleScharnhorst <scharkr1@uw.edu>
2017-09-21 15:57:10 -0700

    Change: dynamic fib to encapsulate its cache structure.
    Didn't need to be a class level element.

--- src/com/fibonacci/DynamicFib.java
+++ src/com/fibonacci/DynamicFib.java
@@ -1,15 +1,14 @@
 package com.fibonacci;
 
 public final class DynamicFib {
-    private static int[] _cache = null;
-
     private DynamicFib(){}
 
     public static int getFibElement(int k) {
         if(k == 0) { return 0; }
         if(k == 1) { return 1; }
 
-        _cache = new int[]{0,1};
+        int[] _cache = new int[]{0,1};
+
         for(int i = 2; i <= k; i++) {
             int temp = _cache[0] + _cache[1];
             _cache[0] = _cache[1];
