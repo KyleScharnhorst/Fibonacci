b73cdf826fb2285cc0792727c5259dbc6a90e393
KyleScharnhorst <scharkr1@uw.edu>
2017-09-21 13:14:32 -0700

    Change: rename main to be more specific.

--- src/com/fibonacci/CompareFibonacci.java
+++ src/com/fibonacci/CompareFibonacci.java
@@ -1,6 +1,7 @@
 package com.fibonacci;
 
-public class Main {
+//compares the speed of a basic recursive solution and
+public class CompareFibonacci {
 
     public static void main(String[] args) {
 	// compare basic recursion vs
