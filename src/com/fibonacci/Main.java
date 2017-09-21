6fbbca3599a9defed56620bae41cc7190012d74a
KyleScharnhorst <scharkr1@uw.edu>
2017-09-21 15:48:41 -0700

    Add: test for recursive fib.

--- src/com/fibonacci/Main.java
+++ src/com/fibonacci/Main.java
@@ -1,5 +1,4 @@
 package com.fibonacci;
-import com.fibonacci.CompareFibonacci.*;
 
 public class Main {
 
@@ -27,11 +26,36 @@ public class Main {
         System.out.println("Time to generate 46th Fibonacci element: " + avg + "ns");
     }
 
+    private static void testRecurseFib(int max_executions) {
+        long before;
+        long after;
+        long avg;
+
+        System.out.println("Recursive fib result: " + RecursiveFib.getFibElement(0));
+        System.out.println("Recursive fib result: " + RecursiveFib.getFibElement(1));
+        System.out.println("Recursive fib result: " + RecursiveFib.getFibElement(2));
+        System.out.println("Recursive fib result: " + RecursiveFib.getFibElement(5));
+        System.out.println("Recursive fib result: " + RecursiveFib.getFibElement(10));
+        System.out.println("Recursive fib result: " + RecursiveFib.getFibElement(46));
+
+        avg = 0;
+        for (int i = 0; i < max_executions; i++) {
+            before = System.nanoTime();
+            RecursiveFib.getFibElement(46);
+            after = System.nanoTime();
+            avg += after - before;
+        }
+        avg /= max_executions;
+
+        System.out.println("Time to generate 46th Fibonacci element: " + avg + "ns");
+    }
+
     public static void main(String[] args) {
         // compare basic recursion vs dynamic fib
         int max_executions = 3;
 
         testDynamicFib(max_executions);
+        testRecurseFib(max_executions);
 
     }
 }
